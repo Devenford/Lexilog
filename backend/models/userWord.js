4f270478c2a1c6016796eed6d76c5647f15b7829
Devenford <khaushldev.ls@gmail.com>
2026-07-17 18:10:10 +0530

    Edited ReadMe.md and Added userWord model schema

---
+++ backend/models/userWord.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+// Each UserWord document is the progress record for one user on one word. Ex: (Alice, brazen)
+
+const userWordSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  word: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Word',
+    required: true
+  },
+  repetitions: {
+    type: Number,
+    default: 0
+  },
+  interval: {
+    type: Number,
+    default: 1 // 1 days
+  },
+  easeFactor: {
+    type: Number,
+    default: 2.5
+  },
+  dueDate: {
+    type: Date,
+    default: Date.now
+  },
+  lastReviewed: {
+    type: Date,
+    default: null
+  },
+  status: {
+    type: String,
+    enum: ['learning', 'mastered'], // The status field is only allowed to have one of these values: 'learning' or 'mastered'
+    default: 'learning'
+  }
+})
+
+/* .index() Ensures each user can have only one progress record per word: (Alice, brazen), (Alice, abate)
+
+It also creates an index to make lookups like: UserWord.findOne({ user, word }) fast.
+*/
+userWordSchema.index(
+  { user: 1, word: 1 },
+  { unique: true }
+)
+
+userWordSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('UserWord', userWordSchema)
